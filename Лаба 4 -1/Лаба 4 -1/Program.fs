e4b5c096a1171cce9a94cff951ada55d467e2e85
mikjur <149616379+mikjur@users.noreply.github.com>
2025-03-20 12:50:35 +0500

    Add files via upload

--- Лаба 4 -1/Лаба 4 -1/Program.fs
+++ Лаба 4 -1/Лаба 4 -1/Program.fs
@@ -1,30 +1,33 @@
 ﻿open System
+
+// Определение типа дерева
 type Tree =
     | Node of string * Tree * Tree 
     | Empty                        
 
+// Функция для применения функции к каждому узлу дерева
 let rec treeMap f tree =
     match tree with
     | Node(data, left, right) ->
         Node(f data, treeMap f left, treeMap f right)
     | Empty -> Empty  
 
-let duplicate (s: string) = s + s
+// Функция для дублирования строки
+let two_word (s: string) = s + s
 
-// Создание бинарного дерева
-let Tree =
-    Node("Март",
-        Node("Январь",
-            Node("Ноябрь", Empty, Empty),  // Левое поддерево: узел со значением "Я"
-            Node("Февраль", Empty, Empty)  // Правое поддерево: узел со значением "Sonik"
-        ),
-        Node("Май",
-            Node("Апрель", Empty, Empty),  // Левое поддерево: узел со значением "Lsp"
-            Node("Июнь", Empty, Empty)  // Правое поддерево: узел со значением "4567"
-        )
-    )
+// Функция для ввода дерева с клавиатуры
+let rec inputTree () =
+    printfn "Введите значение узла (или 'stop' для завершения):"
+    let input = Console.ReadLine()
+    if input = "stop" then
+        Empty
+    else
+        printfn "Введите левое поддерево для узла '%s':" input
+        let left = inputTree ()
+        printfn "Введите правое поддерево для узла '%s':" input
+        let right = inputTree ()
+        Node(input, left, right)
 
-let newTree = treeMap duplicate Tree
 // Функция для вывода дерева
 let rec printTree tree space =
     match tree with
@@ -34,7 +37,17 @@ let rec printTree tree space =
         printTree left (space + 10)
     | Empty -> ()
 
-printfn "Исходное дерево:"
-printTree Tree 0
-printfn "\nДерево с дублированными строками:"
-printTree newTree 0
+// Основная программа
+[<EntryPoint>]
+let main argv =
+    printfn "Введите бинарное дерево:"
+    let tree = inputTree ()  
+
+    printfn "\nИсходное дерево:"
+    printTree tree 0
+
+    let newTree = treeMap two_word tree  
+    printfn "\nДерево с дублированными строками:"
+    printTree newTree 0
+
+    0 // Возвращаем код завершения программы
